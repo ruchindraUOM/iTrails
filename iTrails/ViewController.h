9c8157ae620e67050679eef2281687005d5b0db0
Ruchindra Vithanage <ruchi@Ramyas-MacBook-Pro.local>
2015-07-25 21:23:10 +0530

    Initial Commit

---
+++ iTrails/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iTrails
+//
+//  Created by Ruchindra Vithanage on 7/25/15.
+//  Copyright (c) 2015 Ruchindra Vithanage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iTrails/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iTrails
+//
+//  Created by Ruchindra Vithanage on 7/25/15.
+//  Copyright (c) 2015 Ruchindra Vithanage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
